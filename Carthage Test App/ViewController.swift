fd31df5ada60bfd0d7dc257038a4bba2c0e75caf
Jevin <jevanderson@paypal.com>
2020-02-07 16:14:11 -0800

    Switched to test framework

--- Carthage Test App/ViewController.swift
+++ Carthage Test App/ViewController.swift
@@ -8,17 +8,13 @@
 
 import UIKit
 import Alamofire
+import Carthage_Test_Framework
 
 class ViewController: UIViewController {
   @IBOutlet var header: UILabel!
   @IBOutlet var details: UILabel!
   
   var fetching: Bool = false
-  
-  func randomString(_ length: Int) -> String {
-    let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-    return String((0..<length).map{ _ in letters.randomElement()! })
-  }
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -28,7 +24,7 @@ class ViewController: UIViewController {
   }
   
   func randomizeDetails() {
-    details.text = "Random content: \(randomString(Int.random(in: 100...1000)))"
+    details.text = "Random content: \(cftRandomString(Int.random(in: 100...1000)))"
   }
   
   @IBAction func onClick(_ sender: UIButton, forEvent event: UIEvent) {
